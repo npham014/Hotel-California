a848c2cb4dad763768147cddb4dc5dc226940f56
npham014 <npham014@ucr.edu>
2022-01-21 18:44:24 -0800

    blog_page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import NavBar from "../NavBar/NavBar";
+import Footer from "../Footer/Footer";
+import "./Blog.scss";
 
 function Blog() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar/>
+      <div className="blog--container">
+        
+        <div><h1>Blog</h1></div>
+        <div><p>Insert Blog Posts Here</p></div>
+      </div>
+      <Footer/>
+    </div>
+    );
 }
 
 export default Blog;
